2066c857326300594ce70661fe0ba8b099bb40a9
Dk <danni.kass@gmail.com>
2024-04-03 15:04:13 +0300

    test

---
+++ OOD_Kursprojekt/src/AlbumTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+public class AlbumTest {
+    @Test
+    public void testGetName() {
+
+    }
+
+    @Test
+    public void testGetParentAlbum() {
+
+    }
+
+    @Test
+    public void testGetSubAlbums() {
+
+    }
+}
---
+++ OOD_Kursprojekt/src/MusicOrganizerTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+public class MusicOrganizerTest {
+    @Test
+    public void testAddSoundClip() {
+
+    }
+
+    @Test
+    public void testCreateNewSubAlbum() {
+
+    }
+
+    @Test
+    public void testDeleteSoundClip() {
+
+    }
+
+    @Test
+    public void testDeleteSubAlbum() {
+
+    }
+
+    @Test
+    public void testNotAtRootAlbum() {
+
+    }
+}
